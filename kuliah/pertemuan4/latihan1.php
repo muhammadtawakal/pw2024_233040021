4a697e044c7842060a1d21e0e3818028a16ec0e2
muhammadtawakal66@gmail.com <muhammadtawakal66@gmail.com>
2024-03-10 19:48:03 +0700

    pertemuan4

--- kuliah/pertemuan4/latihan1.php
+++ kuliah/pertemuan4/latihan1.php
@@ -1,9 +1,13 @@
 <?php
 
-echo date("l, d F Y");
+echo date("l, j F Y");
 echo "<br>";
 echo time();
 echo "<br>";
-echo time(); + 86400;
+echo time() + 86400;
 echo "<br>";
-echo date("l" , time() +)
+echo date('l', time() + 86400);
+echo "<br>";
+echo mktime(0, 0, 0, 2, 27, 2024);
+echo "<br>";
+echo date("l", mktime(0, 0, 0, 4, 1, 2005));
